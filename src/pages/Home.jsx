4ff12c45b7a0e7ccb5eba0bcdfb5a4a703479cfc
Takmim00 <takmimm@gmail.com>
2025-02-20 12:09:08 +0600

    socket io added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+
+const Home = () => {
+    const sendMessage+{
+        
+    }
+    return (
+        <div>
+            <input type="text" name="" id="" />
+            <button onClick={sendMessage}>Send Message</button>
+        </div>
+    );
+};
+
+export default Home;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Login from "../pages/authentication/Login";
 import Register from "../pages/authentication/Register";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main/>,
     children:[
+        {
+            path:'/',
+            element:<Home/>
+        },
         {
             path:'/login',
             element:<Login/>
